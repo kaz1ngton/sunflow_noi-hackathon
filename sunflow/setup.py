6fefaa281fa6bd6045df2e68ca5a00a9b015b75f
Fipogh <fipogh@gmail.com>
2023-08-05 12:43:44 +0300

    Create python package for neural network

---
+++ sunflow/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sunflow',
+    version='0.1.0',
+    description='A simple Python package that provides a basic function to add two numbers.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/my_package',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
