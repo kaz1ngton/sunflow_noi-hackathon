45a7fdb334f1230d34a81e3fbe01d1e94e4e34fc
DMITRII_FOROFONTOV <forofontov.dev@gmail.com>
2023-08-05 06:56:36 +0200

    feat: assemble visualization process

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,40 +1,34 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import { FileForm } from '../components/FileForm'
 import { Chart } from '../components/Chart'
-
+import { mapDataset } from '../utils/mapDatasets'
+import { useFirstDatasetSeries, useSecondDatasetSeries } from '../utils/getSeries'
 
 export const Home = () => {
     const [rowFile, setRowFile] = useState(null)
     const [preprocessedFile, setPreprocessedFile] = useState(null)
     const [predictedFile, setPredictedFile] = useState(null)
 
-    const handleSelect = (file) => {
-        const filteredFile = file.filter(Boolean)
-
-        setRowFile(filteredFile)
+    const handleSelect = (data) => {
+        const mappedData = mapDataset(data, 1000)
+        setRowFile(mappedData)
     }
 
     const handleResponse = (data) => {
         setPreprocessedFile(data.preprocessed)
         setPredictedFile(data.predicted)
     }
-
-    const rowChartSeries = [
-        {
-            name: 'Power',
-            valueYField: 'Power',
-            categoryXField: 'Timestamp',
-            reference: useRef(null),
-        },
-    ]
+    const firstDatasetSeries = useFirstDatasetSeries(rowFile, rowFile)
+    const secondDatasetSeries = useSecondDatasetSeries(rowFile, rowFile)
 
     return (
         <div>
             {rowFile ? (
                 <Chart
-                    data={rowFile}
                     yAxisLabel={'Power (W)'}
-                    serieses={rowChartSeries}
+                    serieses={
+                        rowFile[0]['Power'] ? firstDatasetSeries : secondDatasetSeries
+                    }
                     yAxisText={'Power (W)'}
                 />
             ) : (
